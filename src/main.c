1c3fc734150bc287a670ff12f6cd7674a1f32815
Barnold88 <bandslover@outlook.com>
2025-02-10 04:11:16 +0000

    Boilerplate C file added

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// This program aims to find duplicate files by hashing the entire byte array read from a file and comparing hashes with other hashes
+
+
+
+int main(int argc, char *argv[]){
+
+
+
+
+}
