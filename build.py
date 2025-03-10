9fcb33d10fc0cca644a5b6345c9e9e4fded0f2bc
Barnold88 <bandslover@outlook.com>
2025-03-10 23:21:11 +0000

    update build tool to compile for specific operating systems

--- build.py
+++ build.py
@@ -1,9 +1,14 @@
 # too lazy to use cmake or any other build tool, dont need any fancy stuff
 import os
+import platform
+
+binary_name = "main"
+
+binary = f" -o build/{binary_name}.exe" if platform.system().lower() == "windows" else f" -o build/{binary_name}"
 
 source = [file for file in os.listdir("src") if ".c" in file]
 source[0] = "src/" + source[0] # stupid join function grumble grumble grumble
 
-build = "gcc " + " src/".join(source) + " -o build/main.exe"
+build = "gcc " + " src/".join(source) + binary
 
 os.system(build)
