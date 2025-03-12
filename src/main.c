b91aad6caec9d064c0c54e8d70bf9afc197f9cd1
Barnold88 <bandslover@outlook.com>
2025-03-12 18:40:52 +0000

    Main now does all the correct calls and runs in the correct operation order

--- src/main.c
+++ src/main.c
@@ -3,63 +3,20 @@
 #include <stdlib.h>
 #include "args.h"           // for parsing cli args
 #include "fileSystem.h"
-#include "utils.h"
-#include "stringOps.h"
-
-
-#define INPUT_BUFFER 1024
+#include "memory.h"
 
 // This program aims to find duplicate files by hashing the entire byte array read from a file and comparing hashes with other hashes
 
 //TODO: give functionality to delete specific files given idexes,delete all files, delete range, or just quit
 
-void parseInput(){ // move to stringops file
 
-    char userInput[INPUT_BUFFER];
-    int stringLen;
+// Only compatible compilers:
+    // GCC (MinGW-w64)
+    // MSYS2 (GCC-based) 
+    // Cygwin (GCC-based) 
 
-    printf("Enter here: ");
+// Compile on latest version of C for best results (Written in 2025)
 
-    if (fgets(userInput, sizeof(userInput), stdin) != NULL) {
-        userInput[strcspn(userInput, "\n")] = '\0'; 
-    }
-    
-    strArray splitStrings = stringSplit(userInput," ");
-
-    if(splitStrings.capacity <= 0){
-        if(splitStrings.items != NULL) free(splitStrings.items);
-        printf("Error PLACEHOLDER\n");
-        return;
-    }
-
-    if(splitStrings.count != -1){
-        intArray ints = strArrToIntArr(&splitStrings);
-
-        for(int i = 0; i < ints.count; i++){
-            printf("INT[%d] : %d\n",i,ints.items[i]);
-        }
-
-    }
-
-} 
-
-void handleDupes(filePtrArray* files){ // move to filesystem file
-
-    filePtrArray dupes = findDuplicates(files);
-
-    if(files->items != NULL) free(files->items);
-
-    if(dupes.count >=1){
-        printf("===================Duplicates found===================\n\n");
-        for(int i = 0; i < dupes.count; i++){
-            printf("\n\t\t[%d] [FILE_NAME] %s | [FILE_PATH] %s\n\n",i,dupes.items[i].fileName,dupes.items[i].filePath);
-        }
-        printf("\n\nOptions:\n\n\t\tWrite the file numbers (as seen on the left of each found file, to remove them)\n\n\t\tWrite -1 to remove all files\n\n\t\tWrite a range to delete that range of files (n-m) delete files in range n to m\n\n\t\tWrite -2 to quit\n\n\n\n");
-        parseInput();
-    }else{
-        printf("===================No duplicates found===================\n\n");
-    }
-}
 
 int main(int argc, char *argv[]){
 
@@ -71,13 +28,11 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
-    if(validPath(arguments.folderPath)){
-       
+    if(validPath(arguments.folderPath)){   
         filePtrArray files = getFiles(arguments.folderPath, arguments.bufSize);
-        
+    
         if(files.count >=1) handleDupes(&files);
         else printf("===================No files found===================\n\n");
-
     }
 
     else {
