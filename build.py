3c3d7353a290946efb4bd7f7ec9eb721a17a8542
Barnold88 <bandslover@outlook.com>
2025-03-14 15:44:17 +0000

    Add optional compiler args to build via sys.argv[1]

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 # too lazy to use cmake or any other build tool, dont need any fancy stuff
 import os
 import platform
+import sys
+
 
 binary_name = "main"
 
@@ -9,6 +11,9 @@ binary = f" -o build/{binary_name}.exe" if platform.system().lower() == "windows
 source = [file for file in os.listdir("src") if ".c" in file]
 source[0] = "src/" + source[0] # stupid join function grumble grumble grumble
 
-build = "gcc " + " src/".join(source) + binary
+if len(sys.argv) > 1:
+    build = f"gcc {sys.argv[1] } " + " src/".join(source) + binary
+else:
+    build = "gcc " + " src/".join(source) + binary
 
 os.system(build)
