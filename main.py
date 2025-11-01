824d547ccca850e3ceeb6bf04ccc18e035c8e3b7
Barnold88 <bandslover@outlook.com>
2025-11-01 18:50:36 +0000

    Add basic code to begin hashing files with and begin argument parsing logic

---
+++ main.py
@@ -0,0 +1,38 @@
+import hashlib
+import sys
+
+
+def generateHash(fileName,md5,BUFFER_SIZE):
+    # with thanks to https://stackoverflow.com/questions/22058048/hashing-a-file-in-python
+    with open(fileName, 'rb') as f:
+        while True:
+            data = f.read(2**10)
+            if not data:
+                break
+            md5.update(data)
+    return md5.hexdigest()
+
+
+def parseArgs(args):
+
+    args = args[1::]
+
+    if len(args) % 2 == 0:
+        print(args)
+    else:
+        print("ERR")
+        exit()
+
+
+if __name__ == "__main__":
+
+    parseArgs(sys.argv)
+
+    BUFFER_SIZE = 2**10
+    md5 = hashlib.md5()
+    sha1 = hashlib.sha1()
+
+    generateHash("test.txt",md5,sha1)
+
+
+
