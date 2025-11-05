e6e75d0f2a5767003ca2c5ca39ab4781e1fa9b63
Barnold88 <bandslover@outlook.com>
2025-11-05 00:54:09 +0000

    Fix grabFiles to return ABS path of file to avoid relative path errors and add recursive folder search for sub directories

--- DupeFinder.py
+++ DupeFinder.py
@@ -5,8 +5,9 @@ if __name__ == "__main__":
     args = initArgParse()
     # fileGuardRail(args.directory)
     # BUFFER_SIZE = args.buffersize
-    # print(generateHash("./test/test.txt",BUFFER_SIZE))
-    files = generateFileObjs(grabFiles(args.directory))
-    for Obj in files:
-        print(Obj.type)
-    
+
+
+    # files = generateFileObjs(grabFiles(args.directory))
+    # print(groupFiles(files))
+
+    print(grabFiles(args.directory))
--- file.py
+++ file.py
@@ -1,5 +1,5 @@
 from os import listdir
-from os.path import isfile, join, getsize
+from os.path import isfile, isdir, join, getsize,abspath
 
 class File:
    
@@ -8,26 +8,46 @@ class File:
       self.size = size
       self.type = type
 
-
 def grabFiles(path):
-   return [f for f in listdir(path) if isfile(join(path, f))]
+   return [abspath(join(path, f)) for f in listdir(path) if isfile(join(path, f))]
+
+def grabFilesRecursive(path):
+
+   files = []
+   print(path)
+   for f in listdir(path):
+      if isfile(join(path, f)):
+         files.append(f)
+      elif isdir(join(path, f)):
+         files.extend(grabFilesRecursive(join(path, f)))
+
+   return files
+
+
 
 def hasExtension(file):
    parts = file.split(".")
    if len(parts) > 1:
       return len(parts[len(parts)-1]) > 0
    return False
-  
+
+def groupFiles(files):
+   groups = {}
+   for file in files:
+      key = file.type 
+      groups.setdefault(key, []).append(file)
+   return groups
 
 def generateFileObjs(files):
-    fileObjs = []
+   fileObjs = []
 
-    for file in files:
-       fileObj = File(
-          file,
-          getsize(file),
-          file.split(".")[1] if hasExtension(file) else None
-        )
-       fileObjs.append(fileObj)
+   for file in files:
+      fileObj = File(
+         file,
+         getsize(file),
+         file.split(".")[1] if hasExtension(file) else None
+      )
+      fileObjs.append(fileObj)
 
-    return fileObjs
+   return fileObjs
+
